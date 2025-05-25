12f8626cbe2d7f828c4660366605c55f4866774c
Albin Liang <130929541+AlbinJunLiang@users.noreply.github.com>
2025-05-24 23:01:46 -0600

    Update path.js

--- index/functions/path.js
+++ index/functions/path.js
@@ -1,4 +1,4 @@
-const rutaPrincipal = 'C:/Users/50683/Desktop/EscManejo/version4 portafolio/frontend/user/principal/main.html';
+const rutaPrincipal = 'https://albinjunliang.github.io/qtest/main/';
 const ADMIN = "C:/Users/50683/Desktop/EscManejo/version4 portafolio/frontend/admin/principal/index.html";
 
 //uglifyjs scripts.js -o archivo.min.js --compress --mangle --toplevel
